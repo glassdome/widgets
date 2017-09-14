c39601a0922bc0d01ca5edb269e316847b794445
Dorek <dorota@MacBook-Pro.local>
2017-09-13 20:29:13 -0400

    add validations to PostgresWidgetData.scala

--- app/io/glassdome/widgets/services/PostgresWidgetData.scala
+++ app/io/glassdome/widgets/services/PostgresWidgetData.scala
@@ -26,39 +26,50 @@ object PostgresWidgetData extends Database[Widget] {
   
   def create(w: Widget): Try[Widget] = {
     // Validate: Conflict if widget.id already exists.
-    Try { sql"""
+     if (!findById(w.id).isDefined){
+       Try {
+         sql"""
       INSERT INTO widget VALUES(
         ${w.id},
         ${w.name},
         ${w.owner},
         ${w.description})
-      """.update.apply } map { _ => 
-        findById(w.id) getOrElse {
-          throw new RuntimeException("Unknown error creating widget")
-        }
-      }
-  }
+      """.update.apply
+       } map { _ =>
+         findById(w.id) getOrElse {
+           throw new RuntimeException("Unknown error creating widget")
+         }
+       }
+     } else throw ConflictException("Widget IDs must be unique")
+  }//end create
   
   def update(w: Widget): Try[Widget] = {
     // Validate: Conflict if widget does not exist.
-    Try {
-      sql"""
+    if (findById(w.id).isDefined)
+    {
+      Try {
+        sql"""
         UPDATE widget SET
           id = ${w.id},
           name = ${w.name},
           owner = ${w.owner},
           description = ${w.description}
         WHERE id = ${w.id}
-      """.update.apply} map { _ =>
-          findById(w.id) getOrElse {
-            throw new RuntimeException("Unknown error updating widget")
-          }
+      """.update.apply
+      } map { _ =>
+        findById(w.id) getOrElse {
+          throw new RuntimeException("Unknown error updating widget")
+        }
       }
-  }
+    }
+    else throw ConflictException(s"Widget with ID '${w.id}' does not exist.")
+
+  }//end update
+
  
   def delete(id: Int): Try[Widget] = {
     findById(id).fold {
-      throw new WidgetNotFoundException(s"Widget with ID '$id' not found")
+      throw WidgetNotFoundException(s"Widget with ID '$id' not found")
     }{ widget =>
       Try {
         sql"""DELETE FROM widget WHERE id = ${id}""".update.apply
